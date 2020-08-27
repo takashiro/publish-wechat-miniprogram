6c6403c5dba4d2239ba674a59e9a72d52435acc8
Kazuichi Takashiro <takashiro@qq.com>
2020-08-27 22:13:42 +0800

    Catch any error and make CI fail

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,31 @@
 /* eslint-disable no-console */
+import * as core from '@actions/core';
 import * as mp from 'miniprogram-ci';
 
 import Project from './Project';
 
 (async function main(): Promise<void> {
-	const project = new Project();
-	const version = project.getVersion();
-	if (!version) {
-		throw new Error('Please define a version.');
-	}
-
-	if (project.isNpmEnabled()) {
-		const warnings = await mp.packNpm(project);
-		if (warnings.length > 0) {
-			console.warn(warnings);
+	try {
+		const project = new Project();
+		const version = project.getVersion();
+		if (!version) {
+			core.setFailed('Please define a version.');
+			return;
 		}
-	}
 
-	await mp.upload({
-		project,
-		version,
-		onProgressUpdate: console.log,
-	});
+		if (project.isNpmEnabled()) {
+			const warnings = await mp.packNpm(project);
+			if (warnings.length > 0) {
+				console.warn(warnings);
+			}
+		}
+
+		await mp.upload({
+			project,
+			version,
+			onProgressUpdate: console.log,
+		});
+	} catch (error) {
+		core.setFailed(error);
+	}
 }());
